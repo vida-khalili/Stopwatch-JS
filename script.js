8e4502a7654b8d8be96dac28bd9920baf68353d9
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2023-07-02 11:24:53 +0330

    Cookies (set & get)
    set and get Cookies.
    by refreshing the page counter won't change until press reset button.

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let count = 0;
 let countInterval;
 
+function onloadCounter() {
+  count = Number(Cookies.get("count"));
+  document.querySelector(".counter").innerHTML = count;
+
+  let previousSate = Cookies.get("state");
+  if (previousSate === "started") {
+    start();
+  }
+}
+
 function start() {
   countInterval = setInterval(function () {
     count += 1;
     document.querySelector(".counter").innerHTML = count;
     document.querySelector(".start-btn").disabled = true;
+    Cookies.set("count", count);
+    Cookies.set("state", "started");
   }, 1000);
 }
 
 function pause() {
   clearInterval(countInterval);
   document.querySelector(".start-btn").disabled = false;
+  Cookies.set("state", "paused");
 }
 
 function reset() {
@@ -19,4 +32,6 @@ function reset() {
   count = 0;
   document.querySelector(".counter").innerHTML = count;
   document.querySelector(".start-btn").disabled = false;
+  Cookies.set("count", count);
+  Cookies.set("state", "reset");
 }
